41f2c18c05b0d78f9e848789caf1b154f3570b77
ken5scal <kengoscal@gmail.com>
2022-08-19 12:42:03 +0900

    initial ui

--- in-browser-bundling/jbook/src/index.tsx
+++ in-browser-bundling/jbook/src/index.tsx
@@ -1,7 +1,24 @@
+import { useState } from 'react';
 import ReactDom from 'react-dom/client';
 
 const App= () => {
-    return <h1>hi</h1>
+    const [input, setInput] = useState('')
+    const [code, setCode] = useState()
+
+    const onClick = () => {
+        console.log(input)
+    }
+
+    return (
+        <div>
+            <textarea 
+                value={input} 
+                onChange={e => setInput(e.target.value)}>
+            </textarea>
+            <button onClick={onClick}>Submit</button>
+            <pre>{code}</pre>
+        </div>
+    )
 }
 
 ReactDom
