740c065d83c1ed4141c4c3b16a8239c6a2a5242d
ken5scal <kengoscal@gmail.com>
2022-08-19 18:05:02 +0900

    initial ui

--- simple-memo-app/src/components/App.tsx
+++ simple-memo-app/src/components/App.tsx
@@ -4,21 +4,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>簡単メモアプリ</h1>
+      <input type="text"></input>
+      
+      <button>追加</button>
+      <pre>メモ一覧</pre>
     </div>
   );
 }
