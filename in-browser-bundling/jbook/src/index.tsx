74edfde296c9fc07219f444614806f38650de032
ken5scal <kengoscal@gmail.com>
2022-08-21 22:13:10 +0900

    safely handle untrusted code execution

--- in-browser-bundling/jbook/src/index.tsx
+++ in-browser-bundling/jbook/src/index.tsx
@@ -6,8 +6,8 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App= () => {
     const [input, setInput] = useState('')
-    const [code, setCode] = useState()
     const ref = useRef<any>()
+    const iframe = useRef<any>()
 
     const startService = async()=> {
         ref.current = await esbuild.startService({
@@ -23,6 +23,8 @@ const App= () => {
     const onClick = async () => {
         if (!ref.current) return
 
+        iframe.current.srcdoc = html;
+
         const result = await ref.current.build({
             entryPoints: ['index.js'],
             bundle: true,
@@ -33,9 +35,31 @@ const App= () => {
                 'process.env.NODE_ENV': '"production"',
             }
         })
-        console.log(result)
-        setCode(result.outputFiles[0].text)
+
+        // setCode(result.outputFiles[0].text)
+        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
     }
+
+    const html = `
+    <html>
+    <head></head>
+    <body>
+        <div id="root"></div>
+        <script>
+            window.addEventListener('message', (event) => {
+                try {
+                    eval(event.data)
+                } catch (err) {
+                    const root = document.querySelector('#root');
+                    root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+                    console.error(err);
+
+                }
+            }, false)
+        </script>
+    </body>
+    </html>
+    `
 
     return (
         <div>
@@ -44,10 +68,15 @@ const App= () => {
                 onChange={e => setInput(e.target.value)}>
             </textarea>
             <button onClick={onClick}>Submit</button>
-            <pre>{code}</pre>
+            <iframe 
+                ref={iframe}
+                srcDoc={html}
+                sandbox="allow-scripts"
+                title='code preview'/>
         </div>
     )
 }
+
 
 ReactDom
     .createRoot(
