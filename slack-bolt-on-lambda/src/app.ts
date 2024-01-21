75770f0d30482b8f9193fb8fba0434637a317e01
ken5scal <kengoscal@gmail.com>
2024-01-21 12:30:20 +0900

    fix slack

--- slack-bolt-on-lambda/src/app.ts
+++ slack-bolt-on-lambda/src/app.ts
@@ -1,8 +1,13 @@
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
 
+// lambda handler. Only used when called specifically as a lambda function.
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
+    console.log('hogehoghohgeogheo')
+    console.log(context)
+    console.log('Context')
     console.log(`Event: ${JSON.stringify(event, null, 2)}`);
     console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+    console.log('hogehoghohgeogheo')
     return {
         statusCode: 200,
         body: JSON.stringify({
@@ -10,3 +15,9 @@ export const handler = async (event: APIGatewayEvent, context: Context): Promise
         }),
     };
 };
+
+if (process.env.WORKLOAD !== 'lambda') {
+    (async () => {
+        console.log('⚡️ Bolt app is running!');
+    })().catch((error) => console.error(error));
+}
